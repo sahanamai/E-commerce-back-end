7a5cc940b36d8baf0f590f547969a1fff8b3cb88
sahana <sahanamai@gmail.com>
2022-02-07 18:12:17 -0600

    added category and index models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:
+      {
+        //validate value is decimal
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        default: 10,
+        validate: {
+          //validate value is numeric
+        },
+        category_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'category',
+            key: 'id'
+          }
+        }
+      }
+    }
   },
   {
     sequelize,
